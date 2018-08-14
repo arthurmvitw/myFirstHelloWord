90668789aaf73aee61b38ac269db7b04a338443f
ching arthur <arthur.ching@mviptv.com>
2018-08-14 17:09:48 +0800

    Initial Commit

---
+++ HelloWord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWord
+//
+//  Created by arthur kao on 2018/8/14.
+//  Copyright © 2018年 arthur kao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWord
+//
+//  Created by arthur kao on 2018/8/14.
+//  Copyright © 2018年 arthur kao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
