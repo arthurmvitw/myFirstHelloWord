59fc4c3fa65b680209b864d409dd9ee8f91c2925
arthurvhing <arthur.kao@mviptv.com>
2018-08-20 17:02:38 +0800

    add first world

---
+++ HelloWord/FirstHelloWord.h
@@ -0,0 +1,13 @@
+//
+//  FirstHelloWord.h
+//  HelloWord
+//
+//  Created by arthur kao on 2018/8/20.
+//  Copyright © 2018年 arthur kao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstHelloWord : NSObject
+
+@end
---
+++ HelloWord/FirstHelloWord.m
@@ -0,0 +1,13 @@
+//
+//  FirstHelloWord.m
+//  HelloWord
+//
+//  Created by arthur kao on 2018/8/20.
+//  Copyright © 2018年 arthur kao. All rights reserved.
+//
+
+#import "FirstHelloWord.h"
+
+@implementation FirstHelloWord
+
+@end
